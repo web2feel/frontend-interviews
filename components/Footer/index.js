a024c9ae65c05135553c8c6c75f1e22687d70291
Jinson Abraham <jinson@Jinsons-Mac.local>
2021-09-11 21:23:25 +0530

    footer updates

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -2,17 +2,48 @@ import React from "react";
 
 function Footer() {
   return (
-    <div className="px-6 py-12 text-center text-gray-300 text-sm">
+    <div className="px-6 py-12 text-center text-gray-300 text-sm ">
       {" "}
       <p className="animate-pulse text-indigo-400 mb-8 text-lg">
-        <a rel="nofollow" target="_blank" href="https://github.com/web2feel/frontend-interviews/blob/main/CONTRIBUTE.md">Submit a Question</a>
-        
+        <a
+          rel="noreferrer"
+          rel="nofollow"
+          target="_blank"
+          href="https://github.com/web2feel/frontend-interviews/blob/main/CONTRIBUTE.md"
+        >
+          Submit a Question
+        </a>
       </p>
       <p>
-        Developed by Jinson Abraham | Find me on <a rel="nofollow" target="_blank" href="https://github.com/web2feel/frontend-interviews">Github</a>{" "}
+        Developed by Jinson Abraham | Find me on{" "}
+        <a
+          rel="noreferrer"
+          rel="nofollow"
+          target="_blank"
+          href="https://github.com/web2feel/frontend-interviews"
+        >
+          Github
+        </a>{" "}
       </p>
       <p>
-        Built with <a rel="nofollow" target="_blank" href="https://nextjs.org/">NextJS</a> and <a rel="nofollow" target="_blank" href="https://tailwindcss.com/">TailwindCSS</a>
+        Built with{" "}
+        <a
+          rel="noreferrer"
+          rel="nofollow"
+          target="_blank"
+          href="https://nextjs.org/"
+        >
+          NextJS
+        </a>{" "}
+        and{" "}
+        <a
+          rel="noreferrer"
+          rel="nofollow"
+          target="_blank"
+          href="https://tailwindcss.com/"
+        >
+          TailwindCSS
+        </a>
       </p>
     </div>
   );
